2ee66cc33dfff1d74f4e859573e894dac43bbf12
Tom Jarle Christiansen <fuzzbin@gmail.com>
2023-10-05 23:08:24 +0200

    MSAL

--- src/routes/innlogget/+layout.svelte
+++ src/routes/innlogget/+layout.svelte
@@ -1,7 +1,18 @@
 
 
 <script>
-   // MSAL her
+   import { msal } from '$lib/msal.js'
+
+   msal.loginPopup()
+   .then(response => {
+      console.log('Login success:', response);
+   navigate('/dashboard'); // replace with your app's dashboard URL
+  })
+  .catch(error => {
+    console.error('Login error:', error);
+  });
+
+
 </script>
 
 Meny++++
