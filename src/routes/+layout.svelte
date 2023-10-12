41ac8d88881caa06c2073fdd08f2557b64ef6f74
Tom Jarle Christiansen <fuzzbin@gmail.com>
2023-10-12 21:38:35 +0200

    Ombygging

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <nav>
     <a href="/">Hjem</a>
     <a href="/ChatDok">ChatDOK</a>
+    <a href="/innlogget">Innlogget</a>
     <a href="/om">Om</a>
 </nav>
 <slot />
--- src/routes/innlogget/+layout.svelte
+++ src/routes/innlogget/+layout.svelte
@@ -1,7 +1,24 @@
 
 
 <script>
-   
+    import login from '$lib/Auth/login';
+    import { get } from 'svelte/store';
+
+    let msal
+    let user
+
+    console.log('Hello???')
+
+    const loginHandler = async () => {
+        if(!get(userStore)) {
+            const user = await login()
+            userStore.set(user)
+        } else {
+            {}
+        }
+    }
+
+loginHandler();
 
 </script>
 
